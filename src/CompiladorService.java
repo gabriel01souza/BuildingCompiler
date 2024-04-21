85d145994d7cbc0a364967108abdcf36a90444c4
gabriel01souza <gabrielcardososouza3007@gmail.com>
2024-04-21 09:41:23 -0300

    Alterando codigo para ler comentario de linha tambem

--- src/CompiladorService.java
+++ src/CompiladorService.java
@@ -12,52 +12,46 @@ public class CompiladorService {
 
 	public void compilar(String code) {
 		String[] lines = code.split("\n");
-		try {
-			Lexico lexico = new Lexico(code);
-//			Token token1;
-//			token1 = lexico.nextToken();
-//			System.out.println(token1);
-//			Token token2 = lexico.nextToken();
-//			System.out.println(token2);
-//			Token token3 = lexico.nextToken();
-//			System.out.println(token3);
+		lines = limparLinhasVazias(lines);
+		Lexico lexico = new Lexico(code);
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			lexico = new Lexico(line);
+			String[] numberOfWords = line.split(" ");
+			int j = 0; // controle para ao dar o "nextToken"
 
-			for (int i = 0; i < lines.length; i++) {
-				String line = lines[i];
-				lexico = new Lexico(line);
-				String[] numberOfWords = line.split(" ");
-				int j = 0; // controle para ao dar o "nextToken"
-
+			try {
 				Token token = lexico.nextToken();
 				while (Objects.nonNull(token) && j < numberOfWords.length) {
-					System.out.println(token);
+					System.out.println("Linha: " + i + " " + token);
 					if ((j + 1) != numberOfWords.length) {
 						token = lexico.nextToken();
 					}
 					j++;
 				}
+
+			} catch (LexicalError e) {
+				// TODO Auto-generated catch block
+				System.out.println("Erro na linha " + i);
+				e.printStackTrace();
 			}
-
-		} catch (LexicalError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
+	}
 
-//		for (int i = 0; i < lines.length; i++) {
-//			// SEPARA AS LINHAS AQUI, E CONSEGUIMOS TER O CONTROLE NO "i"
-//			String line = lines[i];
-//			Lexico lexico = new Lexico(line.replaceAll(" ", ""));
-//			try {
-//				while (Objects.nonNull(lexico.nextToken())) {
-//					Token token = lexico.nextToken();
-//					tokens.add(token);
-//				}
-//			} catch (LexicalError e) {
-//				System.out.println("Linha: " + i);
-//				e.printStackTrace();
-//			}
-//		}
-		System.out.println(tokens);
+	public String[] limparLinhasVazias(String[] lines) {
+		String[] newLines = new String[lines.length];
+		
+		for (int i = 0; i < lines.length; i++) {
+			String line = lines[i];
+			line = line.replace("\r","");
+			int l = line.length();
+			if (l > 0) {
+				newLines[i] = line;
+			} else {
+				newLines[i] = "";
+			}
+		}
+		return newLines;
 	}
 
 }
